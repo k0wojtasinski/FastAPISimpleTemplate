2cb0cfa3a5fa70c74fc757c96a69747181295cf0
Kacper <k0wojtasinski@gmail.com>
2020-11-26 14:44:32 +0000

    Extended script for setting upndatabase mounts

--- scripts/setup_database_mounts.sh
+++ scripts/setup_database_mounts.sh
@@ -1,5 +1,12 @@
 #/bin/bash
+# This script creates directories to bind containers with database and database_management and sets correct permissions to database data directory
 
-mkdir database_management/data/
-mkdir database/data/
+if [ ! -d "database_management/data/" ]; then
+    mkdir database_management/data/
+fi
+
+if [ ! -d "database/data/" ]; then
+    mkdir database/data/
+fi
+
 chown -R 5050:5050 database_management/data/
